5152da0d70c7f5cd587897acec9cd87c74bd336c
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-01-17 23:51:43 -0300

    feat new components in home-screen, fix marquee

--- app/index.tsx
+++ app/index.tsx
@@ -1,35 +1,90 @@
 import React from "react";
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import Entypo from '@expo/vector-icons/Entypo';
 import {
-  Image,
+  ScrollView,
   ImageBackground,
   StyleSheet,
   Text,
+  TextInput,
   View,
 } from "react-native";
 import { Marquee } from '@animatereactnative/marquee';
 import DESTAQUES from '@/app/utils/homeDestaques';
 
-
 export default function Index() {
   return (
-    <View style={styles.body}>
+    <ScrollView style={styles.body}>
       <Text style={styles.title}>Encontre o corte ideal para você</Text>
 
-      <Marquee spacing={20} speed={0.4} direction="horizontal" style={{ width: "100%", marginTop:20 }}>
-        <View style={styles.container}>
-
-          {DESTAQUES.map((destaque, index) => (
+      <Marquee
+        spacing={20}
+        speed={0.5}
+        direction="horizontal"
+        style={{width: "100%", marginTop: 20}}>
+        <View style={{ flexDirection: "row", alignItems: "center" }}>
+          {DESTAQUES.map((destaque) => (
             <View key={destaque.id} style={styles.card}>
               <ImageBackground
                 source={destaque.img}
                 style={styles.imageContainer}
-                resizeMode="cover">
-              </ImageBackground>
+                resizeMode="cover"
+              />
             </View>
           ))}
         </View>
       </Marquee>
-    </View>
+
+      <View style={styles.inputContainer}>
+        <FontAwesome name="search" size={24} color="#8683A1" />
+        <TextInput placeholderTextColor={"#8683A1"} placeholder="Pesquisar barbeiro, corte de cabelo..." style={styles.inputText} />
+      </View>
+      <Text style={styles.title}>Conheça os nossos profissionais</Text>
+
+      <View style={styles.profissionaisContainer}>
+        <View style={styles.fotoProfissional}></View>
+        <View style={styles.infoProfissinalContainer}>
+          <Text style={[styles.infoText, { fontWeight: "bold" }]}>Nome</Text>
+          <View style={styles.infosRow}>
+            <AntDesign name="star" size={24} color="#8683A1" />
+            <Text style={[styles.infoText, { color: "#8683A1" }]}>Avaliação</Text>
+          </View>
+          <View style={styles.infosRow}>
+            <Entypo name="scissors" size={24} color="#8683A1" />
+            <Text style={[styles.infoText, { color: "#8683A1" }]}>Especializacao</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.profissionaisContainer}>
+        <View style={styles.fotoProfissional}></View>
+        <View style={styles.infoProfissinalContainer}>
+          <Text style={[styles.infoText, { fontWeight: "bold" }]}>Nome</Text>
+          <View style={styles.infosRow}>
+            <AntDesign name="star" size={24} color="#8683A1" />
+            <Text style={[styles.infoText, { color: "#8683A1" }]}>Avaliação</Text>
+          </View>
+          <View style={styles.infosRow}>
+            <Entypo name="scissors" size={24} color="#8683A1" />
+            <Text style={[styles.infoText, { color: "#8683A1" }]}>Especializacao</Text>
+          </View>
+        </View>
+      </View><View style={styles.profissionaisContainer}>
+        <View style={styles.fotoProfissional}></View>
+        <View style={styles.infoProfissinalContainer}>
+          <Text style={[styles.infoText, { fontWeight: "bold" }]}>Nome</Text>
+          <View style={styles.infosRow}>
+            <AntDesign name="star" size={18} color="#8683A1" />
+            <Text style={[styles.infoText, { color: "#8683A1" }]}>Avaliação</Text>
+          </View>
+          <View style={styles.infosRow}>
+            <Entypo name="scissors" size={18} color="#8683A1" />
+            <Text style={[styles.infoText, { color: "#8683A1" }]}>Especializacao</Text>
+          </View>
+        </View>
+      </View>
+
+    </ScrollView>
   );
 }
 
@@ -39,19 +94,52 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     paddingHorizontal: 20,
   },
-  title: { fontSize: 22, fontWeight: "bold", marginTop: 30},
+  title: { fontSize: 22, fontWeight: "bold", marginTop: 30 },
   container: {
     flexDirection: "row",
     alignItems: "center",
-    width: 339,
   },
   card: {
-    marginRight: 10,
-
+    width: 300,
+    marginRight: 20,
   },
   imageContainer: {
-    width: 339,
-    height: 225,
+    width: "100%",
+    height: 200,
     borderRadius: 16,
+    overflow: "hidden",
+  },
+  inputContainer: {
+    width: '100%',
+    borderRadius: 8,
+    marginTop: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 20,
+    paddingVertical: 5,
+    gap: 10,
+    backgroundColor: "#EBF0F5"
+  },
+  inputText: {
+    fontSize: 14,
+    flex: 1
+  },
+  profissionaisContainer: {
+    marginVertical: 10,
+    paddingRight: 20,
+    borderWidth: 1, flexDirection: "row", gap: 10
+
+  },
+  fotoProfissional: {
+    height: 100, width: 100, backgroundColor: "#000", borderRadius: 8,
+  },
+
+  infoProfissinalContainer: {
+    flex: 1, gap: 10
+  },
+  infoText: {
+  },
+  infosRow: {
+    alignItems: "center", flexDirection: "row", gap: 8
   },
 });
--- app/utils/homeDestaques.jsx
+++ app/utils/homeDestaques.jsx
@@ -1,6 +1,9 @@
  const DESTAQUES = [
-    {id: 1, img: require("../assets/images/barber-background2.jpg")},
-    {id: 2, img: require("../assets/images/barber-background.jpg")},
+    {id: 1, img: require("../assets/images/barber-background.jpg")},
+    {id: 2, img: require("../assets/images/barber-background2.jpg")},
+    {id: 3, img: require("../assets/images/barber-background3.jpg")},
+    {id: 4, img: require("../assets/images/barber-background4.jpg")},
+    {id: 5, img: require("../assets/images/barber-background5.jpg")},
 
 ]
 
